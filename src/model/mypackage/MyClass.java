6c145dfda13333b1d6c0f43a4eb459f7869daa33
jamesdrummond <help@genmymodel.com>
2017-08-29 23:31:24 +0000

    commit by jamesdrummond using genmymodel: v20170829_233122

---
+++ src/model/mypackage/MyClass.java
@@ -0,0 +1,49 @@
+package model;
+
+
+/**
+* @generated
+*/
+public class MyClass {
+    
+    /**
+    * @generated
+    */
+    private String attribute;
+    
+    /**
+    * @generated
+    */
+    private Integer attribute2;
+    
+    
+    
+    /**
+    * @generated
+    */
+    public String getAttribute() {
+        return this.attribute;
+    }
+    
+    /**
+    * @generated
+    */
+    public String setAttribute(String attribute) {
+        this.attribute = attribute;
+    }
+    
+    /**
+    * @generated
+    */
+    public Integer getAttribute2() {
+        return this.attribute2;
+    }
+    
+    /**
+    * @generated
+    */
+    public Integer setAttribute2(Integer attribute2) {
+        this.attribute2 = attribute2;
+    }
+    
+}
